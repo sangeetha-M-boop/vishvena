23378d20711207de6226fc90c69f4f89b17b393c
sangeetha-M-boop <sangeethareddy702@gmail.com>
2024-12-18 10:41:06 +0530

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AreaComponent } from './area/area.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+    
+      {path:'contact',component:ContactComponent},
+      {path:'',component:AreaComponent}
+    
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AreaComponent } from './area/area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AreaComponent
   ],
   imports: [
     BrowserModule,
